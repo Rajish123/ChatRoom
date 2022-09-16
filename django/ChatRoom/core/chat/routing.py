b35e8fb7f7cc65674620e91b2eae6550923f16b2
rajish <maharjanrajish101@gmail.com>
2022-09-16 19:51:37 +0545

    endpoints added

--- django/ChatRoom/core/chat/routing.py
+++ django/ChatRoom/core/chat/routing.py
@@ -2,5 +2,7 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+)/$',consumers.ChatRoomConsumer)
+    # w+ is saying anything after chat/ is going to be recognized and picked up 
+    # w = match a word character i.e, match any word with any length, any upper/lower case character that can be alphabets,digits,0-9,underscore character
+    re_path(r'ws/chat/(?P<room_name>\w+)/$',consumers.ChatRoomConsumer.as_asgi())
 ]
